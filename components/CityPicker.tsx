378abbb76bef30cede547eefb0a6f9cd05f16b24
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-05-31 14:57:48 -0500

    install heroicons

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Country, City } from 'country-state-city';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Select from 'react-select';
+import { GlobeAltIcon } from '@heroicons/react/solid';
 
 type option = {
   value: {
@@ -35,10 +37,28 @@ const options = Country.getAllCountries().map((country) => ({
 
 export default function CityPicker() {
   const [selectedCountry, setSelectedCountry] = useState<option>(null);
-  const [selectedCity, setSelectedCityOption] = useState<cityOption>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const router = useRouter();
+
+  function handleSelectedCountry(option: option) {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  }
+
   return (
-    <div>
-      <Select options={options} />
+    <div className='space-y-4'>
+      <div className='space-y-2'>
+        <div className='flex items-center space-x-2 text-white/80'>
+          <GlobeAltIcon className='h-5 w-5 text-white' />
+          <label htmlFor='country'>Country</label>
+        </div>
+        <Select
+          className='text-black'
+          value={selectedCountry}
+          onChange={handleSelectedCountry}
+          options={options}
+        />
+      </div>
     </div>
   );
 }
