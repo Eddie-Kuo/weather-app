50e6d5bca31cfbe7bde9b60c5775e66cf778368c
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-05-30 19:46:19 -0500

    added functionality to dropdown - can select a country

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,9 +1,23 @@
 'use client';
 
 import { Country, City } from 'country-state-city';
+import Select from 'react-select';
+
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}));
 
 interface CityPickerProps {}
 
 export default function CityPicker({}: CityPickerProps) {
-  return <div>CityPicker</div>;
+  return (
+    <div>
+      <Select options={options} />
+    </div>
+  );
 }
