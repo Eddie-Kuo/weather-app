2807bb37e50363d3c23cba92b77775674e0ff43e
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-05-30 19:36:00 -0500

    added landing page ui: title, subtitle, placements of ux

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,16 @@ import { Card, Divider, Subtitle, Text } from '@tremor/react';
 
 export default function Home() {
   return (
-    <main className=''>
-      <Card>
+    <div className='min-h-screen bg-gradient-to-br from-slate-400 to-slate-700 p-10 flex flex-col justify-center'>
+      <Card className='max-w-4xl mx-auto'>
         <Text className='text-5xl font-bold text-center mb-10'>Weather Ai</Text>
+        <Subtitle className='text-xl text-center'>
+          Powered by OpenAI, Next.js 13.4, TailwindCSS, Tremor, and More!{' '}
+        </Subtitle>
+
+        <Divider className='my-10' />
+        <Card className='bg-gradient-to-br from-slate-400 to-slate-700'></Card>
       </Card>
-    </main>
+    </div>
   );
 }
