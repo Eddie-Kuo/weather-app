8309de924630e2f5d46ac43f96c8edb86e5fed02
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-06-01 13:23:52 -0500

    set up stepzen

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function WeatherPage() {
+type Props = {
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
+};
+
+export default function WeatherPage({ params: { city, lat, long } }: Props) {
   return <div>Welcome to the Weather Page</div>;
 }
