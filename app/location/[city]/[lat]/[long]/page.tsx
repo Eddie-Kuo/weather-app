baec67bb6bd3290509622590593e1f75f37b70d7
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-06-01 18:45:46 -0500

    display dashboard header and dynamic timezone

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -29,7 +29,25 @@ export default async function WeatherPage({
   const results: Root = data.myQuery;
 
   console.log(results);
-  console.log('hello');
 
-  return <div>Welcome to the Weather Page</div>;
+  return (
+    <div>
+      {/* Information Panel */}
+
+      <div>
+        <div className='p-5'>
+          <div className='pb-5'>
+            <h2 className='text-xl font-bold'>Todays Overview</h2>
+            <p className='text-sm text-gray-400'>
+              Last Updated at:
+              {new Date(results.current_weather.time).toLocaleString()} (
+              {results.timezone})
+            </p>
+          </div>
+
+          <div>{/* callout card */}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
