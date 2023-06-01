c3f3b7fcc54655f6385e4f39fca2b80dc674acf3
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-06-01 14:44:17 -0500

    fetch weather for city

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import fetchWeatherQuery from '@/graphQl/queries/fetchWeatherQueries';
+import { getClient } from '@/apollo-client';
 
 type Props = {
   params: {
@@ -8,6 +9,27 @@ type Props = {
   };
 };
 
-export default function WeatherPage({ params: { city, lat, long } }: Props) {
+export default async function WeatherPage({
+  params: { city, lat, long },
+}: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: 'true',
+      longitude: long,
+      latitude: lat,
+      timezone: 'CST',
+    },
+  });
+
+  console.log('data', data);
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+  console.log('hello');
+
   return <div>Welcome to the Weather Page</div>;
 }
